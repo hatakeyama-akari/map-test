6c2f946e1f1f818c82fb939a421460faea18a6d9
hatakeyama-akari <hatakeyama@usedge.tech>
2021-03-29 14:44:40 +0900

    display mapbox map view

---
+++ test-map/src/components/map.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect, useState } from "react"
+import mapboxgl from "mapbox-gl"
+import "mapbox-gl/dist/mapbox-gl.css"
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiaGF0YWtleWFtYTAiLCJhIjoiY2tseWdtNzl5MDM2ZzJxcGZtaXliaXR6YiJ9.9TxbJAWYCVPnZEZ5kSy-sw"
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "480px",
+}
+
+const Map = () => {
+  const mapContainerRef = useRef(null)
+
+  const [map, setMap] = useState(null)
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      accessToken: MAPBOX_TOKEN,
+      style: "mapbox://styles/mapbox/streets-v11",
+      // Empire State Building [lng, lat]
+      center: [-73.9856, 40.7497],
+      zoom: 10,
+    })
+    map.addControl(new mapboxgl.NavigationControl(), "top-right")
+
+    setMap(map)
+
+    return () => map.remove()
+  }, [])
+
+  return <div ref={mapContainerRef} style={mapContainerStyle} />
+}
+
+export default Map
--- test-map/src/pages/index.tsx
+++ test-map/src/pages/index.tsx
@@ -3,19 +3,12 @@ import { PageProps, graphql } from "gatsby"
 import { makeStyles } from "@material-ui/core/styles"
 import { Typography, Container, Button, Link } from "@material-ui/core"
 
+import Map from "../components/map"
+
 import Layout from "../components/layout"
 
 const useStyles = makeStyles({
-  container: { margin: 30 },
-  button: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
+  container: {},
 })
 
 type DataProps = {
@@ -28,32 +21,7 @@ const IndexPage: React.FC<PageProps<DataProps>> = ({ data, path }) => {
   const classes = useStyles()
   return (
     <Layout>
-      <Container className={classes.container}>
-        <Typography variant="h3">
-          Gatsby supports TypeScript by default!
-        </Typography>
-        <Typography>
-          This means that you can create and write <em>.ts/.tsx</em> files for
-          your pages, components etc. Please note that the <em>gatsby-*.js</em>{" "}
-          files (like gatsby-node.js) currently don't support TypeScript yet.
-        </Typography>
-        <Typography>
-          For type checking you'll want to install <em>typescript</em> via npm
-          and run <em>tsc --init</em> to create a <em>.tsconfig</em> file.
-        </Typography>
-        <Typography>
-          You're currently on the page "{path}" which was built on{" "}
-          {data.site.buildTime}.
-        </Typography>
-        <Typography>
-          To learn more, head over to our{" "}
-          <Link href="https://www.gatsbyjs.com/docs/typescript/">
-            documentation about TypeScript
-          </Link>
-          .
-        </Typography>
-        <Button className={classes.button}>Styled with Hook API</Button>
-      </Container>
+      <Map />
     </Layout>
   )
 }
